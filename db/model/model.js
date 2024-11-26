dabbcd52f645105112bab7849eca7320d211e31e
Lewis Feely <LewisFeely@Family-Air.northcoders.com>
2024-11-26 12:01:11 +0000

    task 05

--- db/model/model.js
+++ db/model/model.js
@@ -1,19 +1,39 @@
 const db = require("../../db/connection");
+// const articles = require("../data/test-data/articles");
 
 exports.getTopic = () => {
   return db.query("SELECT * FROM topics").then((topics) => {
-    console.log(topics);
     return topics;
   });
 };
 
 exports.getArticleById = (id) => {
-  return db
-    .query(`SELECT * FROM articles WHERE article_id = ${id}`)
-    .then((result) => {
-      if (result.rows.length === 0) {
-        return Promise.reject({ status: 400, msg: "bad request" });
-      }
-      return result;
-    });
+  let queryValues = [];
+  let queryStr = `SELECT * FROM articles`;
+  if (id) {
+    queryValues.push(id);
+    queryStr += ` WHERE article_id = $1`;
+  }
+  queryStr += " ORDER BY created_at";
+
+  return db.query(queryStr, queryValues).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 400, msg: "bad request" });
+    }
+    return rows;
+  });
+};
+
+exports.getArticle = () => {
+  const queryString = `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.article_img_url, articles.votes,
+    CAST(COUNT(comments.comment_id) AS INT) AS comment_count 
+    FROM articles
+    LEFT JOIN comments
+    ON articles.article_id = comments.article_id
+    GROUP BY articles.article_id
+    ORDER BY created_at`;
+
+  return db.query(queryString).then(({ rows }) => {
+    return rows;
+  });
 };
