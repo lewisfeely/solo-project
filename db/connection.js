f6b0d3055c1eabda6031d952ed6973a3fedc3cf9
Lewis Feely <LewisFeely@nas-10-241-8-199.york.ac.uk>
2024-11-28 10:51:49 +0000

    host set up

--- db/connection.js
+++ db/connection.js
@@ -5,14 +5,14 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
-module.exports = new Pool(config);
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE not set");
 }
 
-module.exports = new Pool();
+const config = {};
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
