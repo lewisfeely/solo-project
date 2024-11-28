842bc1d6b7058c8fad4743add2e640229d5b7d08
Lewis Feely <LewisFeely@Family-MacBook-Air.local>
2024-11-28 10:13:05 +0000

    server hosting

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,13 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+module.exports = new Pool(config);
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE not set");
 }
 
